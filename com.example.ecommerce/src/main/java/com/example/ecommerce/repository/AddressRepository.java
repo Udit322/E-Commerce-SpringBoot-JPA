17469b393fee5262e0677610a9aa4df68ac447c2
Pankaj2004s <shubhu2004sharma@gmail.com>
2024-07-02 14:30:29 +0530

    Update AddressRepository.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/repository/AddressRepository.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/repository/AddressRepository.java
@@ -1,5 +1,7 @@
 package com.example.ecommerce.repository;
 
-public interface AddressRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.ecommerce.entity.Address;
 
+public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
