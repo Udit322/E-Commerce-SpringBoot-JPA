305029a841183a878807ffb4d91d90d6da769c99
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-02 20:00:34 +0530

    Update Cartitem.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Cartitem.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Cartitem.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -8,6 +9,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name="cart-items-details")
 public class Cartitem {
+	@Id
 	private Integer cartItemId;
 
     @ManyToOne
