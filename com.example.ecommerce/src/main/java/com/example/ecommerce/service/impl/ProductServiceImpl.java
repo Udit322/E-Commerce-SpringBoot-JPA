5150157af17dc1f3b86bd6ff8cd2e5eb6467ffcc
Sachinjain3100 <sj8692769@gmail.com>
2024-07-02 14:36:39 +0530

    Update ProductServiceImpl.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/ProductServiceImpl.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/ProductServiceImpl.java
@@ -1,5 +1,52 @@
 package com.example.ecommerce.service.impl;
 
-public class ProductServiceImpl {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.ecommerce.entity.Product;
+import com.example.ecommerce.repository.ProductRepository;
+import com.example.ecommerce.service.ProductService;
 
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Integer id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Integer id, Product product) {
+        Product existingProduct = productRepository.findById(id).orElse(null);
+        if (existingProduct != null) {
+            existingProduct.setProductName(product.getProductName());
+            existingProduct.setPrice(product.getPrice());
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setManufacturer(product.getManufacturer());
+            existingProduct.setQuantity(product.getQuantity());
+            existingProduct.setCategory(product.getCategory());
+            existingProduct.setStatus(product.getStatus());
+            existingProduct.setSeller(product.getSeller());
+            return productRepository.save(existingProduct);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Integer id) {
+        productRepository.deleteById(id);
+    }
 }
