9624d90cde5ee5fa5541318927b472d97bcd8322
Pankaj2004s <shubhu2004sharma@gmail.com>
2024-07-02 14:34:14 +0530

    Update CartRepository.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/repository/CartRepository.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/repository/CartRepository.java
@@ -1,5 +1,7 @@
 package com.example.ecommerce.repository;
 
-public interface CartRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.ecommerce.entity.Cart;
 
+public interface CartRepository extends JpaRepository<Cart, Integer> {
 }
