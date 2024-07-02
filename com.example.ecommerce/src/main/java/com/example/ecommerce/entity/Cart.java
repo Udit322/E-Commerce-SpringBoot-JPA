7bf5848553acf5a66bf0afdb150612046af418a2
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-02 20:00:04 +0530

    Update Cart.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Cart.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Cart.java
@@ -2,12 +2,13 @@ package com.example.ecommerce.entity;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
-
+@Entity
 public class Cart {
 
 	@Id
