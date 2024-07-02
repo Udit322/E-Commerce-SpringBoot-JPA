710d238815725b800d21bfeeff3ab6ac2730ed82
Pankaj2004s <shubhu2004sharma@gmail.com>
2024-07-02 14:34:44 +0530

    Update CustomerRepository.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/repository/CustomerRepository.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.example.ecommerce.repository;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.ecommerce.entity.Customer;
 
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
