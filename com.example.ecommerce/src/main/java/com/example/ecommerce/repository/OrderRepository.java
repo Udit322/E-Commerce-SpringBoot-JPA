5323e94c1eacd72e96ace3b893c8fd4116aefe43
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-03 18:48:13 +0530

    Update OrderRepository.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/repository/OrderRepository.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/repository/OrderRepository.java
@@ -1,7 +1,17 @@
 package com.example.ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.example.ecommerce.entity.Customer_Order;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Customer_Order, Integer> {
+    
+    @Query("SELECT o FROM Customer_Order o WHERE o.customer.customerId = :customerId")
+    List<Customer_Order> findByCustomerId(@Param("customerId") Integer customerId);
 }
