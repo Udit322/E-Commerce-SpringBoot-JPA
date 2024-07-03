8a826a71e595acffe2004f4387d0eb8e5fa1fcb3
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-03 18:47:34 +0530

    Update OrderStatus.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/entity/OrderStatus.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/entity/OrderStatus.java
@@ -1,10 +1,7 @@
 package com.example.ecommerce.entity;
 
 public enum OrderStatus {
-	
-	    Cancelled,
-	    Confirmed,
-	    Dispatched,
-	    Delivered
+	PENDING, PROCESSING, SHIPPED, DELIVERED, CANCELED;
 	
 }
+
