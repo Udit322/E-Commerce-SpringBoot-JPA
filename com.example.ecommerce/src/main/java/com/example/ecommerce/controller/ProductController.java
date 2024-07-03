d003f04bc9bc561744f3f5db852718476b0122b9
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-03 18:45:31 +0530

    Update ProductController.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -1,3 +1,55 @@
+//package com.example.ecommerce.controller;
+//
+//import java.util.List;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import com.example.ecommerce.entity.Product;
+//import com.example.ecommerce.service.ProductService;
+//
+//@RestController
+//@RequestMapping("/api/products")
+//public class ProductController {
+//
+//    @Autowired
+//    private ProductService productService;
+//
+//    @GetMapping("/all")
+//    public List<Product> getAllProducts() {
+//        return productService.getAllProducts();
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Product> getProductById(@PathVariable Integer id) {
+//        Product product = productService.getProductById(id);
+//        if (product != null) {
+//            return ResponseEntity.ok(product);
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
+//
+//    @PostMapping("/addpro")
+//    public Product addProduct(@RequestBody Product product) {
+//        return productService.addProduct(product);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Product> updateProduct(@PathVariable Integer id, @RequestBody Product product) {
+//        Product updatedProduct = productService.updateProduct(id, product);
+//        if (updatedProduct != null) {
+//            return ResponseEntity.ok(updatedProduct);
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteProduct(@PathVariable Integer id) {
+//        productService.deleteProduct(id);
+//        return ResponseEntity.noContent().build();
+//    }
+//}
+
 
 package com.example.ecommerce.controller;
 
