63f98b6e0231b7f2a0fbc1b21bfb10d6a3768e28
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-03 18:47:06 +0530

    Update Customer_Order.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Customer_Order.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Customer_Order.java
@@ -3,6 +3,8 @@ package com.example.ecommerce.entity;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -14,100 +16,106 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "customer_orders")
 public class Customer_Order {
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderId;
     private LocalDate orderDate;
+
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
     private Double total;
     private String cardNumber;
 
     @ManyToOne
     @JoinColumn(name = "customer_id")
+    @JsonIgnore
     private Customer customer;
 
     @OneToMany
+    @JsonIgnore
     private List<Cartitem> orderedItems;
 
-    public Integer getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(Integer orderId) {
-		this.orderId = orderId;
-	}
-
-	public LocalDate getOrderDate() {
-		return orderDate;
-	}
-
-	public void setOrderDate(LocalDate orderDate) {
-		this.orderDate = orderDate;
-	}
-
-	public  OrderStatus getOrderStatus() {
-		return orderStatus;
-	}
-
-	public void setOrderStatus(OrderStatus orderStatus) {
-		this.orderStatus = orderStatus;
-	}
-
-	public Double getTotal() {
-		return total;
-	}
-
-	public void setTotal(Double total) {
-		this.total = total;
-	}
-
-	public String getCardNumber() {
-		return cardNumber;
-	}
-
-	public void setCardNumber(String cardNumber) {
-		this.cardNumber = cardNumber;
-	}
-
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
-
-	public List<Cartitem> getOrderedItems() {
-		return orderedItems;
-	}
-
-	public void setOrderedItems(List<Cartitem> orderedItems) {
-		this.orderedItems = orderedItems;
-	}
-
-	public Address getOrderAddress() {
-		return orderAddress;
-	}
-
-	public void setOrderAddress(Address orderAddress) {
-		this.orderAddress = orderAddress;
-	}
-
-	@OneToOne
+    @OneToOne
+    @JsonIgnore
     private Address orderAddress;
 
-	public Customer_Order() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    // Getters and Setters
 
-    public void setPaymentMethod(Customer_Order string) {
-		// TODO Auto-generated method stub
-	Customer_Order paymentmethod = string ;
-	}
+    public Integer getOrderId() {
+        return orderId;
+    }
 
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Cartitem> getOrderedItems() {
+        return orderedItems;
+    }
+
+    public void setOrderedItems(List<Cartitem> orderedItems) {
+        this.orderedItems = orderedItems;
+    }
+
+    public Address getOrderAddress() {
+        return orderAddress;
+    }
+
+    public void setOrderAddress(Address orderAddress) {
+        this.orderAddress = orderAddress;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        // Implementation for setting payment method
+    }
+
+    public String getPaymentMethod() {
+        // Implementation for getting payment method
+        return null;
+    }
 }
