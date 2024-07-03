4cba30338dcd7a4ee90e407b72b3c38b8bf3bef6
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-03 18:50:18 +0530

    Add files via upload

---
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/OrderService.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.ecommerce.entity.Customer_Order;
+
+public interface OrderService {
+    Customer_Order createOrder(Customer_Order order);
+    List<Customer_Order> getAllOrders();
+    Customer_Order getOrderById(Integer orderId);
+    List<Customer_Order> getOrdersByCustomerId(Integer customerId);
+    Customer_Order updateOrderStatus(Integer orderId, Customer_Order order);
+    void deleteOrder(Integer orderId);
+    Optional<Customer_Order> getOrderHistory(Integer customerId);
+}
