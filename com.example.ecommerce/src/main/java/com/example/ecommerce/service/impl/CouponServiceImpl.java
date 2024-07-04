7d916bfd3ec9b1368e12f29b3afd28f9efc1176c
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 21:26:01 +0530

    Update CouponServiceImpl.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/CouponServiceImpl.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/CouponServiceImpl.java
@@ -1,5 +1,25 @@
-package com.example.ecommerce.service.impl;
 
-public class CouponServiceImpl {
+package com.example.ecommerce.serviceimpl;
 
+import com.example.ecommerce.entity.Coupon;
+import com.example.ecommerce.repository.CouponRepository;
+import com.example.ecommerce.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CouponServiceImpl implements CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Override
+    public Coupon createCoupon(Coupon coupon) {
+        return couponRepository.save(coupon);
+    }
+
+    @Override
+    public Coupon getCouponByCode(String code) {
+        return couponRepository.findByCode(code);
+    }
 }
