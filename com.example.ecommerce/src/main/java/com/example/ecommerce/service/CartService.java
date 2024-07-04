71d96c57341834a8ce344ca069065be0c89308aa
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-04 17:16:38 +0530

    Update CartService.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/service/CartService.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/CartService.java
@@ -3,6 +3,8 @@ package com.example.ecommerce.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.example.ecommerce.entity.Cart;
 
 public interface CartService {
@@ -10,10 +12,10 @@ public interface CartService {
     List<Cart> getCart();
 
    Cart addToCart(Cart cart);
-   Cart getCartById(Integer cartId);
+   ResponseEntity<Cart> getCartById(Integer cartId);
 //    void removeFromCart(Long customerId, Long productId);
 //
 //    void updateCartQuantity(Long customerId, Long productId, Integer quantity);
 
-    Cart deleteCart(Integer cartId);
+    ResponseEntity<Cart> deleteCartById(Integer cartId);
 }
