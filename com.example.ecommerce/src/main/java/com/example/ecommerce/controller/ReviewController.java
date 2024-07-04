3a7c3c1e1940cc11f81bfc9cf79c093279e6d98e
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 20:31:44 +0530

    Add files via upload

---
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/controller/ReviewController.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.entity.Review;
+import com.example.ecommerce.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping("/add")
+    public Review addReview(@RequestBody Review review) {
+        return reviewService.addReview(review);
+    }
+
+    @GetMapping("/product/{productId}")
+    public List<Review> getReviewsByProductId(@PathVariable Integer productId) {
+        return reviewService.getReviewsByProductId(productId);
+    }
+}
