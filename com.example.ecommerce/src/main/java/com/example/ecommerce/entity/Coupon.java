cd19c2605cdd242383a790b62dcb54a43eb744b0
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 21:18:16 +0530

    Update Coupon.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Coupon.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Coupon.java
@@ -1,7 +1,13 @@
 package com.example.ecommerce.entity;
 
-import javax.persistence.*;
+
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "coupons")
