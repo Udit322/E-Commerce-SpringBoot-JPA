af71798c522b7983f48c5b1a0c2c07bae521db0f
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 20:52:10 +0530

    Update ReviewService.java

--- ReviewService.java
+++ ReviewService.java
@@ -1,10 +1,1 @@
-package com.example.ecommerce.service;
 
-import com.example.ecommerce.entity.Review;
-
-import java.util.List;
-
-public interface ReviewService {
-    Review addReview(Review review);
-    List<Review> getReviewsByProductId(Integer productId);
-}
