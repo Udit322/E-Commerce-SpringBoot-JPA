b46e207de95660c6df09dcaf6ac15c782c6d3e68
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 21:12:45 +0530

    Update WishlistController.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/controller/WishlistController.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/controller/WishlistController.java
@@ -1,5 +1,31 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.entity.Wishlist;
+import com.example.ecommerce.service.WishlistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/wishlist")
 public class WishlistController {
 
+    @Autowired
+    private WishlistService wishlistService;
+
+    @PostMapping("/add")
+    public Wishlist addToWishlist(@RequestBody Wishlist wishlist) {
+        return wishlistService.addToWishlist(wishlist);
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public List<Wishlist> getWishlistByCustomerId(@PathVariable Integer customerId) {
+        return wishlistService.getWishlistByCustomerId(customerId);
+    }
+
+    @DeleteMapping("/remove/{wishlistId}")
+    public void removeFromWishlist(@PathVariable Integer wishlistId) {
+        wishlistService.removeFromWishlist(wishlistId);
+    }
 }
