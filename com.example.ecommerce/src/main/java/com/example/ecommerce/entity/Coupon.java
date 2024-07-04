1b09ace05c9488522281141cb888f3e7d3501b08
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 21:17:33 +0530

    Update Coupon.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Coupon.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Coupon.java
@@ -1,5 +1,17 @@
 package com.example.ecommerce.entity;
 
-public class Coupon {
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "coupons")
+public class Coupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer couponId;
+    private String code;
+    private Double discount;
+    private LocalDateTime expiryDate;
+
+    // getters and setters
 }
