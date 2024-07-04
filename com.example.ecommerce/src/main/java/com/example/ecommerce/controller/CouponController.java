adbc4375b7eb40142e5c169bd6bb14e903ac0c64
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 21:29:13 +0530

    Update CouponController.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/controller/CouponController.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/controller/CouponController.java
@@ -1,1 +1,25 @@
 
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.entity.Coupon;
+import com.example.ecommerce.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/coupons")
+public class CouponController {
+
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping("/create")
+    public Coupon createCoupon(@RequestBody Coupon coupon) {
+        return couponService.createCoupon(coupon);
+    }
+
+    @GetMapping("/{code}")
+    public Coupon getCouponByCode(@PathVariable String code) {
+        return couponService.getCouponByCode(code);
+    }
+}
