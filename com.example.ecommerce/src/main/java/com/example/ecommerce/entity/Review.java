9d1d767a9ccf64465a0968ac12b92201a600cad5
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 20:33:13 +0530

    Add files via upload

---
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/entity/Review.java
@@ -0,0 +1,74 @@
+package com.example.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reviewId;
+    private String reviewContent;
+    private Integer rating;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+	public Integer getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(Integer reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public String getReviewContent() {
+		return reviewContent;
+	}
+
+	public void setReviewContent(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Review() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+    
+}
