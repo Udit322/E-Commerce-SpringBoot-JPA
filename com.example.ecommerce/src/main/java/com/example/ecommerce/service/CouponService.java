f9028eba7eed271b11e7b7bdce61b73ca9e2b09e
saurabhsaini <160897917+saurabh80001@users.noreply.github.com>
2024-07-04 21:23:48 +0530

    Update CouponService.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/service/CouponService.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/CouponService.java
@@ -1,5 +1,8 @@
 package com.example.ecommerce.service;
 
-public interface CouponService {
+import com.example.ecommerce.entity.Coupon;
 
+public interface CouponService {
+    Coupon createCoupon(Coupon coupon);
+    Coupon getCouponByCode(String code);
 }
