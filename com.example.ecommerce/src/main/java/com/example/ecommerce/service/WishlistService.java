a2b7a561dd6d4924f312059f462e3f80df397cd8
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-05 10:09:35 +0530

    Update WishlistService.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/service/WishlistService.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/WishlistService.java
@@ -5,7 +5,7 @@ import com.example.ecommerce.entity.Wishlist;
 import java.util.List;
 
 public interface WishlistService {
-    Wishlist addToWishlist(Wishlist wishlist);
+    Wishlist addToWishlist(Wishlist wishlist, Integer productId, Integer customerId);
     List<Wishlist> getWishlistByCustomerId(Integer customerId);
     void removeFromWishlist(Integer wishlistId);
 }
