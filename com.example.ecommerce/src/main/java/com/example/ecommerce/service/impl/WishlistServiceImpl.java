85b24e004cdd93c156cc85adff221e2d49d08769
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-05 10:10:47 +0530

    Update WishlistServiceImpl.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/WishlistServiceImpl.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/WishlistServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.ecommerce.service.impl;
 
+import com.example.ecommerce.entity.Customer;
+import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.entity.Wishlist;
+import com.example.ecommerce.repository.CustomerRepository;
+import com.example.ecommerce.repository.ProductRepository;
 import com.example.ecommerce.repository.WishlistRepository;
 import com.example.ecommerce.service.WishlistService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +17,23 @@ public class WishlistServiceImpl implements WishlistService {
 
     @Autowired
     private WishlistRepository wishlistRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Override
-    public Wishlist addToWishlist(Wishlist wishlist) {
-        return wishlistRepository.save(wishlist);
+    public Wishlist addToWishlist(Wishlist wishlist, Integer productId, Integer customerId) {
+//        return wishlistRepository.save(wishlist);
+    	  Product product = productRepository.findById(productId)
+                  .orElseThrow(() -> new RuntimeException("Product not found"));
+          Customer customer = customerRepository.findById(customerId)
+                  .orElseThrow(() -> new RuntimeException("Customer not found"));
+          wishlist.setProduct(product);
+          wishlist.setCustomer(customer);
+          return wishlistRepository.save(wishlist);
     }
 
     @Override
