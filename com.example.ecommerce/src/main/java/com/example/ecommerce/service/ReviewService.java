4a384ef81c005f2c4381b825d7f6153f5c5023a6
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-05 10:09:57 +0530

    Update ReviewService.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/service/ReviewService.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/ReviewService.java
@@ -5,6 +5,6 @@ import com.example.ecommerce.entity.Review;
 import java.util.List;
 
 public interface ReviewService {
-    Review addReview(Review review);
+    Review addReview(Review review, Integer productId, Integer customerId);
     List<Review> getReviewsByProductId(Integer productId);
 }
