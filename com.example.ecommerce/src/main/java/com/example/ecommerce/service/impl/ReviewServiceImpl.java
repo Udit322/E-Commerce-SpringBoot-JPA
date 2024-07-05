29a23915caaa2809b57b66d41cfdd3df58dece34
Udit322 <148511920+Udit322@users.noreply.github.com>
2024-07-05 10:11:09 +0530

    Update ReviewServiceImpl.java

--- com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/ReviewServiceImpl.java
+++ com.example.ecommerce/src/main/java/com/example/ecommerce/service/impl/ReviewServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.ecommerce.service.impl;
 
+import com.example.ecommerce.entity.Customer;
+import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.entity.Review;
+import com.example.ecommerce.repository.CustomerRepository;
+import com.example.ecommerce.repository.ProductRepository;
 import com.example.ecommerce.repository.ReviewRepository;
 import com.example.ecommerce.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +17,25 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Autowired
     private ReviewRepository reviewRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Override
-    public Review addReview(Review review) {
-        return reviewRepository.save(review);
-    }
+    public Review addReview(Review review, Integer productId, Integer customerId) {
+//        return reviewRepository.save(review);
+    
+    	 
+    	   
+    	        Product product = productRepository.findById(productId)
+    	                .orElseThrow(() -> new RuntimeException("Product not found"));
+    	        Customer customer = customerRepository.findById(customerId)
+    	                .orElseThrow(() -> new RuntimeException("Customer not found"));
+    	        review.setProduct(product);
+    	        review.setCustomer(customer);
+    	        return reviewRepository.save(review);
+    	    }
 
     @Override
     public List<Review> getReviewsByProductId(Integer productId) {
